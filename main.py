85aa916fb0992f8f813b764d40ceb2105da6a2f5
Amandeep Singh <aman20dec@gmail.com>
2023-02-13 22:42:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from http.client import MOVED_PERMANENTLY
+import os
+import shutil
+from tkinter import Button, Entry, Label, Tk
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+
+def copy_folders():
+    root_path = ".../../GARY"
+    target = "/2020"
+    folder_list = [ folder[0] for folder in os.walk(root_path) if target in folder[0]]
+
+    for folder in folder_list:
+        dest_path = folder.replace("/Bannerz Work/", "/Test/")
+        shutil.copytree(folder, dest_path)
+
+          
+
+window = Tk()
+window.title("Copy Folder")
+window.config(padx=50, pady= 50, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+
+### textbox for root directory
+label_root = Label(text="Root Directory")
+label_root.grid(column=1, row=1)
+
+txt_root = Entry(width=40)
+txt_root.grid(column=2, row=1)
+#### TODO Add folder finder buttons to each row
+
+### Entry for target location
+label_target_loc = Label(text="Target Folder")
+label_target_loc.grid(column=1, row=2)
+
+txt_target = Entry(width=40)
+txt_target.grid(column=2, row=2)
+
+
+### Entry for target forlder
+label_target = Label(text="Dest. Directory")
+label_target.grid(column=1, row=3)
+
+txt_target = Entry(width=40)
+txt_target.grid(column=2, row=3)
+
+### Start Button
+
+btn_start = Button(text="Copy Folder(s)", command= copy_folders)
+
+btn_start.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
+
+
+
+#TODO full path of the root directory where to start walk from
+# C:\...\Root_Directory\
+
+#TODO get only target folder name .i.e no back or front slashes
+
+#TODO Path of the Target directory where to save
+# C:\...\Target, E:\...\Target
+
+#TODO label to show status
+# traversing folders, Moving folder from x to y, x folder already exists
+
+#TODO Also print the status on the terminal
